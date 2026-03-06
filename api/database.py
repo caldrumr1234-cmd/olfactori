76bf279980ce1151d90a2f07c6f616da6f7465dd
Adam <caldrumr1234@gmail.com>
2026-03-05 19:02:15 -0800

    Fix SQLite threading issue

--- api/database.py
+++ api/database.py
@@ -14,6 +14,7 @@ def get_db():
     con.row_factory = sqlite3.Row
     con.execute("PRAGMA journal_mode=WAL")
     con.execute("PRAGMA foreign_keys=ON")
+    con = sqlite3.connect(DB_PATH, check_same_thread=False)
     try:
         yield con
     finally:
