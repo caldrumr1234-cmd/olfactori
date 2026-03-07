970af46ce85842782c78be767fbae2bd646d6c8d
Adam <caldrumr1234@gmail.com>
2026-03-07 11:46:02 -0800

    Fix CORS: replace wildcard origin with explicit allowed origins

--- api/main.py
+++ api/main.py
@@ -11,7 +11,12 @@ app = FastAPI(title="Olfactori API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://www.olfactori.vip",
+        "https://olfactori.vip",
+        "http://localhost:5173",
+        "http://localhost:4173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
